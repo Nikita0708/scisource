1435675926d36a44e871ee83adeb0fc5ba278559
Nikita0708 <nreva97@gmail.com>
2023-10-07 09:17:42 +0200

    okay

--- src/index.js
+++ src/index.js
@@ -11,13 +11,13 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistedStore}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistedStore}>
+                <BrowserRouter basename="/scisource">
+                    <App />
+                </BrowserRouter>
+            </PersistGate>
+        </Provider>
+    </React.StrictMode>
 );
