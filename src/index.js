d72c0a4448583688afda19459c90f5e741aa9c81
Nikita0708 <nreva97@gmail.com>
2023-10-08 22:00:35 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <BrowserRouter basename="/">
+        <BrowserRouter basename="nikita0708.github.io/scisource">
             <App />
         </BrowserRouter>
     </React.StrictMode>
